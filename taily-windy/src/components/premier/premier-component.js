bd6a7fd6d64bae7d9578b97736b39512e43f52a7
kiritocode1 <kathawalearyan9@gmail.com>
2022-01-20 11:24:41 +0530

    make happy

--- taily-windy/src/components/premier/premier-component.js
+++ taily-windy/src/components/premier/premier-component.js
@@ -13,10 +13,11 @@ const Premier = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
-        slidesToScroll: 2,
-        autoplay: false,
-        Initialslide:0
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    arrows: true,
+    autoplay: true,
+
     };
     const PremierImages = [
         {
@@ -46,15 +47,26 @@ const Premier = () => {
             title: "the Sinister -Season 1",
             subtitle: "English",
             isDark:false
-        }
+        }, {
+            src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC/et00316141-munvacnnhm-portrait.jpg",
+            alt: "the Sinister -Season 1",
+            title: "the Sinister -Season 1",
+            subtitle: "English",
+            isDark:false
+        },{
+            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC/et00122532-rvwldkjbqk-portrait.jpg",
+            alt: "venom",
+            title: "Venom-2:let there be carnage",
+            subtitle: "English",
+            isDark:false
+            
+        },
     ]
     return (
         <>
-        <Slider {...settings}>
-        {PremierImages.map((image)=>(<Poster {...image}/>))}
-        </Slider>
-        
-        
+            <Slider {...settings}>
+                {PremierImages.map((images)=>(<Poster {...images}/>))}
+            </Slider>
         </>
     );
 }; 
