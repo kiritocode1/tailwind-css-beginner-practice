5db64732a8ca5bf4dd6d34bd302dff0f2c10b6ed
kiritocode1 <kathawalearyan9@gmail.com>
2022-01-21 12:16:08 +0530

    gg baby

--- taily-windy/src/components/Pages/Movie.page.js
+++ taily-windy/src/components/Pages/Movie.page.js
@@ -3,9 +3,11 @@ import MovieNavbar from "../Movie.navbar-component";
 const Movie = () => { 
 
     return (<>
-        <MovieNavbar/>
-        <div className="flex items-center justify-center w-full h-full"><img src="https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/pushpa--the-rise-et00129538-08-12-2021-01-21-46.jpg" alt="ok man whatever" className="h-full w-full " /></div>
-        
+        <MovieNavbar />
+        <div>
+        <div className="w-full h-96 bg-gradient-to-r from-green-900 text-green-500 z-20 absolute">kjlskdjlk</div>
+        <img src="https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/pushpa--the-rise-et00129538-08-12-2021-01-21-46.jpg" alt="jnkjfkj" className="w-full h-96 z-10 absolute"/>
+        </div>
     </>);
 };
 
